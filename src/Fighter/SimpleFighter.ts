09d6217d0d002e7c94a055e1d28b1dbbf2818ac3
NyPadilha <763npp@gmail.com>
2023-11-02 22:16:58 -0300

    feat: interface SimpleFighter

---
+++ src/Fighter/SimpleFighter.ts
@@ -0,0 +1,8 @@
+export default interface SimpleFighter {
+  strength: number;
+  lifePoints: number;
+  
+  attack(enemy: SimpleFighter): void;
+  
+  receiveDamage(attackPoints: number): number;
+}
